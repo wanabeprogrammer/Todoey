629b05ee890076cbf0d3ce2a49abcc937bbc27bf
Robert Bounds <rob@roberts-macbook-pro-2.local>
2018-09-15 15:27:28 -0500

    Convert to MVC pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoList.swift
+//  Todoey
+//
+//  Created by Robert Bounds on 9/15/18.
+//  Copyright © 2018 Robert Bounds. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+
+}
